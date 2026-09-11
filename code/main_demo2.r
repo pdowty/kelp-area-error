4ae9e78e2e6fc209404249d574ec4e0daca6848f
Pete Dowty <peter.dowty@dnr.wa.gov>
2026-09-11 16:44:47 -0700

    Added calc of correlations in demo2.

--- code/main_demo2.r
+++ code/main_demo2.r
@@ -24,7 +24,7 @@ p2_mean <- 12
 p2_stdev <- 3
 
 # correlation values to create range of covariance between random variables 
-corr_vals <- c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0)
+corr_design_vals <- c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0)
 
 
 
@@ -38,8 +38,9 @@ z1 <- rnorm(N)   # vector of standard normal random variates
 data <- data.frame(x1 = x1_mean + z1*x1_stdev)
 
 # create multiple series of x2 vectors with different covariance with x1
-for (icorr in corr_vals) {
- x2 <- icorr * data$x1 + sqrt(1 - icorr^2) * rnorm(N)
+for (icorr in corr_design_vals) {
+ x2_parent <- p2_mean + rnorm(N)*p2_stdev
+ x2 <- icorr * data$x1 + sqrt(1 - icorr^2) * x2_parent 
  corr_str <- sprintf("%.1f", icorr)
  suffix <- str_remove(corr_str, "\\.")
  x2name <- str_c("x2", suffix, sep="_")
@@ -51,6 +52,7 @@ for (icorr in corr_vals) {
 ###############################################################################
 #  get x1-x2 correlations
 ###############################################################################
+corr_vals <- cor(data[,1], data[,2:7], use = "pairwise.complete.obs")
 
 
 
